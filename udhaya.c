979b38d9a88048885a8c6cdac331200d12981133
udhayakumar137 <35800598+udhayakumar137@users.noreply.github.com>
2018-02-01 15:58:46 +0530

    Create udhaya.c

---
+++ udhaya.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+voidmain()
+{
+double number;
+printf("enter a number");
+scanf("%dif",&number);
+if (number<=0)
+{
+if (nmumber==0)
+printf("you entered 0");
+else
+printf("enter a negative number");
+}
+else
+printf("enter a positive number");
+return 0;
+}
