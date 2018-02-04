369cf0f3b0a2fc3d034d2f5703fedd63be56ac0e
udhayakumar137 <35800598+udhayakumar137@users.noreply.github.com>
2018-02-04 16:03:51 +0530

    Create market uk.c

---
+++ market uk.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    char *a1,*a2,*a3;
+    printf("the product are\n1.dairymilk,\n2.rose,\n3.creatingcart");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case1:
+        printf("%d",&a1);
+        break;
+        case2:
+        printf("%d",&a2);
+        break;
+        case3:
+        printf("%d",&a3);
+        break;
+        default:   
+        printf("no stacks available");
+    }
+    }
